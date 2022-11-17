29df6a64894670f5fc8a573e933e1ac0e7e8aaef
tejaspundlik <tejaspundlik2@gmail.com>
2022-11-17 14:21:50 +0530

    done

--- test.py
+++ test.py
@@ -1,1 +1,29 @@
-print("Hello World")
+target=int(input())
+lenofnum=int(input())
+for i in range(lenofnum):
+    questionarr.append(int(input()))
+
+arr=[]
+number=15
+
+def decToBinary(number,LEN):
+    arr=[]
+    for i in range(0,LEN):
+        arr.append(number%2)
+        number=number//2
+
+    arr.reverse()
+    return arr
+
+answer=0
+for k in range(0,pow(2,len(questionarr))-1):
+ Sum=0
+ arr=decToBinary(k,lenofnum)
+ for i in range(0,len(questionarr)):
+    if(arr[i]==0):
+        Sum=Sum+questionarr[i]
+    else:
+        Sum=Sum-questionarr[i]
+ if(Sum==target):
+    answer+=1
+print(answer)
